4154afde29f5abebfa4cd9defb526db84303cd6f
Pascal Klaßen <p.klassen221199@gmail.com>
2021-02-26 22:45:23 +0100

    Add first Node prototype

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" variant="dark" type="dark">
+    <b-navbar variant="dark" type="dark">
       <b-navbar-brand href="#">Pathfinding Visualizer</b-navbar-brand>
       <b-button-group>
         <b-dropdown variant="outline-light" text="Select Maze"></b-dropdown>
-        <b-dropdown variant="outline-light" text="Select Algorithm"></b-dropdown>
+        <b-dropdown
+          variant="outline-light"
+          text="Select Algorithm"
+        ></b-dropdown>
       </b-button-group>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item
+          href="https://github.com/pascalklassen/pathfinding-visualizer/"
+          target="_blank"
+        >
+          <b-icon icon="github" title="View on GitHub" variant="light"></b-icon>
+          GitHub
+        </b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
-    <div class="container">
-      <b-jumbotron header="Prototype" lead="This is still a prototype!">
-        <p>For more information click the button below</p>
-        <b-button variant="outline-success">More Info</b-button>
-      </b-jumbotron>
-    </div>
+    <div class="container"></div>
   </div>
 </template>
 
 <script>
+//let nodes = [];
+
+/* for (let i = 0; i < 400; i++) {
+  nodes.push(new GridNode());
+} */
+
 export default {
-  name: 'App',
-}
+  name: "App",
+  data: function() {
+    return {
+      nodes: [],
+    };
+  },
+};
 </script>
 
-<style>
-</style>
+<style scoped></style>
